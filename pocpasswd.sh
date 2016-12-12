de4791639128d52976110012c5a85503a37e8680
Klaus Reuter <khr@rzg.mpg.de>
2016-12-12 15:53:10 +0100

    created pocpasswd

---
+++ pocpasswd.sh
@@ -0,0 +1,10 @@
+# Helper function to allow to set the password for poc as environment variable
+# without having it appear in bash history.
+# Source ("source") this file into your bash session and then call the function.
+
+function pocpasswd() {
+    echo -n " Enter your OwnCloud password (will not be echoed/visible): "
+    read -s PASSWORD
+    echo
+    export POC_PASSWORD="$PASSWORD"
+}
